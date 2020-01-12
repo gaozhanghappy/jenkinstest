e3a82a866b4b139c73759a5390cb9a3c2d1372aa
Administrator <303812918@qq.com>
2020-01-12 22:10:52 +0800

    测试jenkins

--- src/main/java/git/gaozhang/demo/DemoApplication.java
+++ src/main/java/git/gaozhang/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
     @ResponseBody
     @RequestMapping("/")
     public String helloWorld(){
-        return "hello world222";
+        return "hello world";
     }
 
     public static void main(String[] args) {
